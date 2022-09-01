9b2d767dcebe362a21cd91825031a15daabca0e5
Librariann <okpc0306@naver.com>
2022-09-01 19:58:29 +0900

    [test, e2e] login.tsx testing complete

--- cypress/e2e/firstTest/first-test.cy.ts
+++ cypress/e2e/firstTest/first-test.cy.ts
@@ -1,32 +1,35 @@
 describe("Log In", () => {
+  const login = cy;
   it("should see login page", () => {
-    cy.visit("/").title().should("eq", "Login | Ruber Eats");
-  });
-
-  it("can fill out the form", () => {
-    cy.visit("/")
-      .get("body")
-      .findByPlaceholderText(/email/i)
-      .type("testadmin@gmail.com")
-
-      .get("body")
-      .findByPlaceholderText(/password/i)
-      .type("tjdgus123")
-
-      .get("body")
-      .findByRole("button")
-      .should("not.have.class", "pointer-events-none");
-    // todo (can log in)
+    login.visit("/").title().should("eq", "Login | Ruber Eats");
   });
 
   it("can see email / password validation errors", () => {
-    cy.visit("/")
-      .get("body")
-      .findByPlaceholderText(/email/i)
-      .type("bad@email")
+    login.visit("/");
+    login.findByPlaceholderText(/email/i).type("bad@email");
+    login.findByRole("alert").should("have.text", "Please enter a valid email");
+    login.findByPlaceholderText(/email/i).clear();
+    login.findByRole("alert").should("have.text", "Email is required");
+    login.findByPlaceholderText(/email/i).type("bad@email.com");
+    login
+      .findByPlaceholderText(/password/i)
+      .type("a")
+      .clear();
+    login.findByRole("alert").should("have.text", "Password is required");
+  });
 
-      .get("body")
-      .findByRole("alert")
-      .should("have.text", "Please enter a valid email");
+  it("can fill out the form", () => {
+    login.visit("/");
+    login.findByPlaceholderText(/email/i).type("testadmin@gmail.com");
+    login.findByPlaceholderText(/password/i).type("tjdgus123");
+    login
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none")
+      .click();
+    login.window().its("localStorage.ruber-token").should("be.a", "string");
+  });
+
+  it("sign up", () => {
+    login.visit("/create-account");
   });
 });

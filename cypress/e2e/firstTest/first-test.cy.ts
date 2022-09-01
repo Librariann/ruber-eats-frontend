3d5264000083a3a1609776ce4f5d6f892e61cc7c
Librariann <okpc0306@naver.com>
2022-09-01 19:46:03 +0900

    [test, commit] cypress settings2

--- cypress/e2e/firstTest/first-test.cy.ts
+++ cypress/e2e/firstTest/first-test.cy.ts
@@ -1,7 +1,32 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "Login | Ruber Eats");
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Ruber Eats");
+  });
+
+  it("can fill out the form", () => {
+    cy.visit("/")
+      .get("body")
+      .findByPlaceholderText(/email/i)
+      .type("testadmin@gmail.com")
+
+      .get("body")
+      .findByPlaceholderText(/password/i)
+      .type("tjdgus123")
+
+      .get("body")
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none");
+    // todo (can log in)
+  });
+
+  it("can see email / password validation errors", () => {
+    cy.visit("/")
+      .get("body")
+      .findByPlaceholderText(/email/i)
+      .type("bad@email")
+
+      .get("body")
+      .findByRole("alert")
+      .should("have.text", "Please enter a valid email");
   });
 });
